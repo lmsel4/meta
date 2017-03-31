ed410d0e41c75e01a4737bc4e1c9e42c1a89dc12
Ogier Bouvier <ogier.bouvier@epfl.ch>
2017-03-31 14:46:42 +0200

    updated dummy module for slab testing

--- apps/module-loader/src/hello.c
+++ apps/module-loader/src/hello.c
@@ -1,16 +1,56 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/slab.h>
+
+/**
+ * Dummy module for testing functionnality ported from Linux
+ **/
+
+struct hello_mod {
+    int value;
+    char* name;
+};
+
+static void* ptr;
+static hello_mod* array;
 
 int hello_init(void)
 {
-	printk(KERN_INFO "Hello world\n");
+    static void* zero = NULL;
+    printk(KERN_INFO "Hello world\n");
 
-	return 0;
+    ptr = kmalloc(sizeof(struct hello_mod), GFP_ATOMIC);
+
+    if (!ptr)
+    {
+        printk(KERN_CRIT, "Could not allocate memory\n");
+        return -1;
+    }
+
+    array = kcalloc(sizeof(struct hello_mod), 10, GFP_ATOMIC);
+
+    if (!array)
+    {
+        printk(KERN_CRIT, "Unable to allocate array memory!\n");
+        return -1;
+    }
+
+    zero = kzalloc(sizeof(uint64_t));
+
+    if (*((uint64_t*) zero) != 0)
+    {
+        printk(KERN_CRIT, "kzalloc allocated non-zero memory!\n");
+        return -1;
+    }
+
+    return 0;
 }
 
 void hello_exit(void)
 {
-	printk(KERN_INFO "Goodbye world\n");
+    kfree(ptr);
+    kfree(array);
+    printk(KERN_INFO "Goodbye world\n");
 }
 
 module_init(hello_init);
