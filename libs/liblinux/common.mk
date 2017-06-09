1c379293021b8523a357b9ee3c8eea672fb1be1b
tharvik <tharvik@users.noreply.github.com>
2017-06-09 13:31:19 +0200

    merge common Makefile defs

---
+++ libs/liblinux/common.mk
@@ -0,0 +1,26 @@
+# base
+CFLAGS += -D__KERNEL__ -std=gnu89
+CFLAGS += -Wno-implicit-function-declaration -Wno-incompatible-pointer-types
+CFLAGS += -Wno-pointer-sign -Wno-shift-count-overflow
+
+# CONFIG_*
+CFLAGS += -DCONFIG_X86_32=y -DCONFIG_TREE_RCU=y
+CFLAGS += -DCONFIG_MCORE2=y -DCONFIG_HZ=1000
+CFLAGS += -DCONFIG_X86_L1_CACHE_SHIFT=6
+
+# TODO find correct include path defining these
+CFLAGS += -DBITS_PER_LONG=32 -D__LITTLE_ENDIAN 
+CFLAGS += -DHZ=1000 -DMSEC_PER_SEC=1000
+
+# some always needed files, TODO too much magic 
+CFLAGS += -include linux/kconfig.h -include align.h -include linux/types.h
+CFLAGS += -include seL4/types.h
+CFLAGS += -include asm/msr-index.h
+CFLAGS += -include $(srctree)/include/generated/autoconf.h
+
+# magic to add the correct Linux header
+__BASE := $(objtree)/stage/x86/pc99/include/liblinux_include
+CFLAGS += -I$(__BASE)
+CFLAGS += -I$(__BASE)/x86
+CFLAGS += -I$(__BASE)/x86/uapi
+CFLAGS += -I$(__BASE)/uapi
